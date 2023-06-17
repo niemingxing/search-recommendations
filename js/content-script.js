3097fbef62b2626126725faa632887f7a54cb525
homeniemingxing <94492352@qq.com>
2023-06-17 11:17:03 +0800

    init

---
+++ js/content-script.js
@@ -0,0 +1,152 @@
+﻿(function() {
+	let kwIndex = 0;
+	// 创建一个空字符串用于拼接 CSV 内容
+	let csvContent = "";
+	let keywords = '';
+	let keywordList = [];
+	let currentDomain = window.location.hostname;
+
+	function getNeetElement($type)
+	{
+		let result;
+		if($type == "search")
+		{
+			if(currentDomain.includes("douyin"))
+			{
+				result = document.querySelector('header form input[type=text]');
+			}
+			else if(currentDomain.includes("xiaohongshu"))
+			{
+				result = document.querySelector('.search-input');
+			}
+			else if(currentDomain.includes("bilibili"))
+			{
+				result = document.querySelector('.nav-search-input');
+				console.log(result);
+			}
+
+		}
+		else if($type == "recommend")
+		{
+			if(currentDomain.includes("douyin"))
+			{
+				result = document.querySelectorAll("header div[data-index]");
+			}
+			else if(currentDomain.includes("xiaohongshu"))
+			{
+				result = document.querySelectorAll("div.sug-item");
+			}
+			else if(currentDomain.includes("bilibili"))
+			{
+				result = document.querySelectorAll("div.suggestions div.suggest-item");
+			}
+
+		}
+
+		return result;
+	}
+
+	/**
+	 * 搜索关键词
+	 * @param keywords
+	 */
+	function search(q)
+	{
+		let searchInput = getNeetElement("search");
+		if (searchInput) {
+			let inputEvent = new InputEvent('input', {
+				bubbles: true,
+				cancelable: true,
+				inputType: 'insertText',
+				data: q
+			});
+			let changeEvent = new Event('change', {
+				bubbles: true,
+				cancelable: true
+			});
+			let keyUpEvent = new KeyboardEvent('keyup', {
+				key: '',
+				bubbles: true,
+				cancelable: true
+			});
+			searchInput.value = q;
+			searchInput.focus();
+			searchInput.dispatchEvent(inputEvent);
+			searchInput.dispatchEvent(changeEvent);
+			searchInput.dispatchEvent(keyUpEvent);
+
+			setTimeout(function() {
+				let recommendElements = getNeetElement("recommend");
+				// 遍历每个 div 元素并输出其 TEXT 内容
+				recommendElements.forEach(function(element) {
+					let html = element.innerHTML;
+					let text = html.replace(/<[^>]*>/g, "");
+					csvContent += text + "\n";
+					console.log(text);
+				});
+				kwIndex++;
+				if(kwIndex >= keywordList.length)
+				{
+					saveCsv(csvContent);
+					return;
+				}
+				searchByCharCode();
+			}, 3000); // 3000 毫秒等于 3 秒
+		}
+	}
+
+	/**
+	 * 保存内容为csv文件
+	 * @param csvContent
+	 */
+	function saveCsv(csvContent)
+	{
+		// 创建一个 Blob 对象，将内容保存为 CSV 文件
+		var blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+
+		// 生成一个临时下载链接并下载文件
+		var link = document.createElement("a");
+		link.href = URL.createObjectURL(blob);
+		link.download = "data(" + currentDomain+ ").csv";
+		link.click();
+	}
+
+	function searchByCharCode()
+	{
+		let kw = keywordList[kwIndex];
+		search(kw);
+	}
+
+	chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+		if (message.keywords) {
+			// 将关键词信息展示在页面的搜索框中
+			console.log(message)
+			window.focus();
+			csvContent = "";
+			keywords = message.keywords;
+			// 修改表头为 "推荐关键词"
+			let headers = ["推荐关键词"];
+			csvContent += headers.join(",") + "\n";
+
+			let hasNewline = keywords.includes("\n");
+			if(hasNewline)
+			{
+				keywordList = keywords.split("\n").filter(function(item) {
+					return item.trim() !== "";
+				});
+			}
+			else
+			{
+				keywordList.push(keywords);
+				for (let i = 97; i <= 122; i++) {
+					let character = String.fromCharCode(i);
+					keywordList.push(keywords + character);
+				}
+			}
+			//console.log(keywordList);
+			searchByCharCode();
+		}
+	});
+
+})();
+
---
+++ js/popup.js
@@ -0,0 +1,12 @@
+﻿$("#submit").click(function (){
+   let keywords = $("#keywords").val();
+   if(keywords.trim() == '')
+   {
+      alert("输入不可以为空！");
+      return;
+   }
+    // 向 content-scripts.js 发送消息，包含关键词信息
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { keywords: keywords , closePopup: true});
+    });
+});
